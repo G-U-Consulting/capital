af459cdc1ebfd89ce5371f00f86ff74517ba5225
Johan Cortes <johandcortes@gmail.com>
2025-04-28 11:43:25 -0500

    Registro auditoria

--- wwwroot/js/own/axios-helper.js
+++ wwwroot/js/own/axios-helper.js
@@ -25,8 +25,28 @@ axios.defaults.maxRedirects = true;
 axios.defaults.validateStatus = function (status) { return true; };
 async function httpFunc(path, data) {
     var resp = await axios.post(path, data);
-    if (resp.status == 200)
+    if (resp.status == 200) {
+        if(path.includes('/genericST/')){ 
+            operacion = path.split(':')[1];
+            if (operacion.includes('Upd') || operacion.includes('Ins') || operacion.includes('Del')){
+                registro = {
+                    operacion,
+                    datos: JSON.stringify(data),
+                    username: GlobalVariables.username
+                };
+                let res = operacion.includes('Ins') && resp.data.data.split('-')[1] ? resp.data.data.split('-')[1] : null;
+                if (res) {
+                    resp.data.data = 'OK';
+                    let datos = {...data};
+                    datos[res.split(':')[0]] = res.split(':')[1];
+                    registro.datos = JSON.stringify(datos);
+                }
+                await axios.post('/generic/genericST/Auditoria:Ins_Auditoria', registro);
+            }
+        }
+        
         return resp.data;
+    }
     console.log(resp);
     //if (resp.status == 401)
     //if (resp.status == 403) 
