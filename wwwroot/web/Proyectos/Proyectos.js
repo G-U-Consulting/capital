7c3ba610027b5c7a6e514c8ef2297bb2dbcbb7a6
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2025-04-29 21:45:19 -0500

    Integración del módulo de Informes y Cargues con ajustes de estilos

--- wwwroot/web/Proyectos/Proyectos.js
+++ wwwroot/web/Proyectos/Proyectos.js
@@ -175,7 +175,28 @@
                 "Información adicional",
                 "Enlaces"
             ],
-            casoValidator: []
+            casoValidator: [],
+            //ejemplo informe cargue
+            selectCarg: "",
+            fileSelected: null,
+            selectInfo: "",
+            parsInforme: {
+                pars: [
+                  { parametro: 'Fecha Desde', tipo: 'date', valor: '', visible: true },
+                  { parametro: 'Fecha Hasta', tipo: 'date', valor: '', visible: true },
+                  { parametro: 'Sala de Negocios:', tipo: 'select', valor: '', opciones: [
+                    'Alameda de Zipaquirá',
+                    'Mystique 106',
+                    'Porto Hayuelos',
+                    'Urbania',
+                    'Serralta'
+                  ], visible: true },
+                  { parametro: 'Asesor:', tipo: 'select', valor: '', opciones: ['Asesor1', 'Aesor2'], visible: true },
+                  
+                ],
+                parsLen: 2,
+                resultadoEjecucion: null
+              }
         };
     },
     computed: {
@@ -189,7 +210,7 @@
               return 'wizarTabIncomplete';
             }
           });
-        }
+        },
     },
     async mounted() {
         this.tabsIncomplete = this.tabs.map((_, index) => index);
@@ -217,6 +238,14 @@
                 this.pie_legal = resp[8];
                 this.fiduciaria = resp[9];
                 this.bancos = resp[10];
+            }
+            if(mode == 4){
+                var resp = await httpFunc("/generic/genericDS/General:Get_Informes", {});
+                resp = resp.data;
+
+                this.informes = resp[0];
+                this.cargues = resp[1];
+
             }
             this.mainmode = mode;
             this.mode = 0;
@@ -445,6 +474,16 @@
         },
         hasPermission(id) {
             return !!GlobalVariables.permisos.filter(p => p.id_permiso == id).length;
+        },
+        /////// Informes ////////////
+        handleFileUpload(event) {
+            const file = event.target.files[0];
+            if (file) {
+                this.fileSelected = file;
+            }
+        },
+        removeFile() {
+            this.fileSelected = null;
         }
     },    
 };
