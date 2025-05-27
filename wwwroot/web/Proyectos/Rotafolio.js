09c88c9f4da3e5afd2182113f64e0c5991c9f576
Johan Cortes <johandcortes@gmail.com>
2025-05-27 13:19:25 -0500

    Rotafolio

--- wwwroot/web/Proyectos/Rotafolio.js
+++ wwwroot/web/Proyectos/Rotafolio.js
@@ -11,17 +11,32 @@ export default {
             loading: true,
             stop: true,
             interval: null,
+            timeout: null,
             playIndex: 0,
+            showBar: false,
+            vplayer: null,
+            time: 8000,
+            cont: null
         };
     },
     async mounted() {
         this.proyecto = await GlobalVariables.miniModuleCallback("Rotafolio", null);
         this.setMainMode('Rotafolio');
+        var tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        await this.setTime();
         await this.listResources();
     },
     methods: {
         setMainMode(mode) {
             this.mainmode = mode;
+        },
+        async setTime() {
+            let res = await httpFunc('/generic/genericDT/General:Get_Variable', {nombre_variable: 'CarDurac'});
+            if (res.data.length && res.data[0].valor) 
+                this.time = parseFloat(res.data[0].valor.replace(',','.')) * 1000;
         },
         async listResources() {
             this.loading = true;
@@ -40,7 +55,7 @@ export default {
                 this.addResources(await httpFunc('/generic/genericDT/Maestros:Get_Archivos',
                     { tipo: 'logo,slide,planta', id_proyecto }));
 
-                let modulos = ['imagenes','videos','recorridos virt','obras'];
+                let modulos = ['imagenes','videos','recorridos virt','avances de obra'];
                 res = await httpFunc('/generic/genericDT/Medios:Get_GrupoProyecto', { id_proyecto });
                 let grupos = res.data.sort((a, b) => parseInt(a.orden) - parseInt(b.orden));
                 res = await httpFunc('/generic/genericDT/Maestros:Get_Archivos',
@@ -55,10 +70,7 @@ export default {
                 });
 
                 this.loadResources();
-                console.log(this.files);
             }
-
-            this.loading = false;
         },
         addResources(res) {
             let data = res.data;
@@ -70,19 +82,25 @@ export default {
             try {
                 let files = [...this.files];
                 await Promise.all(files.map(async (f, i) => {
-                    const res = await fetch('/file/S3get/' + f.path);
-                    if (!res.ok) throw new Error(`Error al cargar ${f.path}: ${res.statusText}`);
-                    const blob = await res.blob(),
-                        file = new File([blob], f.name, { type: blob.type }),
-                        reader = new FileReader();
-                    reader.onload = async (e) => 
-                        file.type.startsWith('image/') && (this.files[i].content = e.target.result);
-                    reader.readAsDataURL(file);
+                    if (f.link) {
+                        let link = this.formatURLYouTube(f.link); // f.link;
+                        this.files[i].link = link;
+                    }
+                    else {
+                        const res = await fetch('/file/S3get/' + f.path);
+                        if (!res.ok) throw new Error(`Error al cargar ${f.path}: ${res.statusText}`);
+                        const blob = await res.blob(),
+                            file = new File([blob], f.name, { type: blob.type }),
+                            reader = new FileReader();
+                        reader.onload = async (e) => 
+                            file.type.startsWith('image/') && (this.files[i].content = e.target.result);
+                        reader.readAsDataURL(file);
+                    }
                 }));
-                console.log(this.files);
             } catch (error) {
                 console.error("Error al cargar archivos:", error);
             }
+            this.loading = false;
         },
         fullScreen() {
             let cont = document.getElementById("cont-rotafolio");
@@ -114,8 +132,63 @@ export default {
             if (this.interval) clearInterval(this.interval);
             this.interval = setInterval(() => {
                 this.playIndex = (this.playIndex + 1) % this.files.length;
-            }, 5000);
+            }, this.time);
             this.stop = false;
-        }
+        },
+        formatURLYouTube(url) {
+            try {
+                let urlObj = new URL(url);
+                if (urlObj.hostname.includes("youtube.com") && urlObj.searchParams.has("v")) {
+                    let videoID = urlObj.searchParams.get("v");
+                    return `https://www.youtube.com/embed/${videoID}`;
+                } else if (urlObj.hostname.includes("youtu.be")) {
+                    let videoID = urlObj.pathname.substring(1);
+                    return `https://www.youtube.com/embed/${videoID}`;
+                } else return url;
+            } catch(e) {
+                showMessage(e.message.includes('Invalid URL') 
+                    ? 'Algunos videos no se cargaron: link invalido' 
+                    : e.message);
+                return null;
+            }
+        },
+        isVideo(item, next) {
+            if (item && !this.loading) {
+                if (!item.content && item.link) {
+                    if (this.interval) clearInterval(this.interval);
+                    this.interval = null;
+                    return true;
+                } else {
+                    next && this.setIndex(1);
+                }
+            } else return false;
+        },
+        onPlayerStateChange(e) {
+            if (e.data === YT.PlayerState.ENDED) this.setIndex(1);
+        },
+        initVideo(e) {
+            this.vplayer = new YT.Player("v-player", {
+                events: {
+                    "onStateChange": this.onPlayerStateChange
+                }
+            });
+            setTimeout(() => {
+                let state = this.vplayer.getPlayerState();
+                if (state !== YT.PlayerState.PLAYING) {
+                    this.vplayer.mute();
+                    this.vplayer.playVideo();
+                }
+            }, 1000);
+        },
+        setShowBar() {
+            if (!this.cont) this.cont = document.getElementById('cont-rotafolio');
+            this.showBar = true;
+            this.timeout && clearTimeout(this.timeout);
+            this.cont.style.cursor = 'auto';
+            this.timeout = setTimeout(() => {
+                this.showBar = false;
+                this.cont.style.cursor = 'none';
+            }, 2500);
+        },
     }
 }
