3082e1370c006427836481543bf861b1a7b74c4a
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2025-05-27 11:15:52 -0500

    feat(): recorridos virtuales

--- SQL/Medios/Del_Archivos.sql
+++ SQL/Medios/Del_Archivos.sql
@@ -12,11 +12,11 @@ update fact_documento_proyecto
 set is_active = 0
 where
     id_proyecto = @id_proyecto
-    and tipo like concat('%', @tipo, '%')
+    and tipo = @tipo
     and id_grupo_proyecto = @id_grupo_proyecto
     and (
         @id_maestro_documento is null
-        or id_maestro_documento = @id_maestro_documento
+        or id_maestro_documento <=> @id_maestro_documento
     );
 
 select 'ok' as result;
