8593b6a32fd72ba2ca7e30190ac71c5e5efb0dbf
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2025-06-06 21:06:10 -0500

    feat(): Registros - Pro - Nego

---
+++ SQL/ProcesoNegocio/Get_Registro.sql
@@ -0,0 +1,54 @@
+﻿-- =============================================
+-- Proceso: ProcesoNegocio/Get_Registro
+-- =============================================
+--START_PARAM
+set @cliente = '',
+    @id_visita = '';
+--END_PARAM
+
+select a.id_visita,
+       a.id_cliente,
+       a.id_categoria_medio,
+       a.id_medio,
+       a.id_motivo_compra,
+       a.id_referencia,
+       a.presupuesto_disponible,
+       a.otro_texto,
+       e.motivo_compra as motivo,
+       f.referencia as referencia,
+       a.created_on as fecha,
+       g.nombre as proyecto,
+       a.descripcion,
+       (
+        select group_concat(id_tipo_registro)
+        from fact_tipo_registro
+        where id_visita = a.id_visita
+      ) as tipo_registro,
+      (
+        select group_concat(id_modo_atencion)
+        from fact_modo_atencion
+        where id_visita = a.id_visita
+      ) as modo_atencion,
+      (
+        select group_concat(c.tipo_registro)
+        from fact_tipo_registro b
+        join dim_tipo_registro c on b.id_tipo_registro = c.id_tipo_registro
+        where b.id_visita = a.id_visita
+      ) as nombre_tipo_registro,
+      (
+        select group_concat(d.modo_atencion)
+        from fact_modo_atencion b
+        join dim_modo_atencion d on b.id_modo_atencion = d.id_modo_atencion
+        where b.id_visita = a.id_visita
+      ) as nombre_modo_atencion
+from fact_visitas a
+join fact_clientes b on a.id_cliente = b.id_cliente
+join dim_motivo_compra e on a.id_motivo_compra = e.id_motivo_compra
+join dim_referencias f on a.id_referencia = f.id_referencia
+join fact_proyectos g on a.id_proyecto = g.id_proyecto
+where a.is_active = 1 and (
+  b.numero_documento = @cliente
+  or a.id_visita = @id_visita
+)
+order by a.created_on desc
+
--- SQL/Querys/2025/2025-06/20250601.sql
+++ SQL/Querys/2025/2025-06/20250601.sql
@@ -25,11 +25,107 @@ create table fact_clientes (
 	created_by varchar(200) default current_user
 );
 
+create table dim_tipo_registro (
+    id_tipo_registro int auto_increment primary key,
+    tipo_registro varchar(100),
+    is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+
+create table dim_modo_atencion (
+    id_modo_atencion int auto_increment primary key,
+    modo_atencion varchar(100),
+    is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+
+create table dim_motivo_compra (
+    id_motivo_compra int auto_increment primary key,
+    motivo_compra varchar(100),
+    is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+
+create table dim_referencias (
+    id_referencia int auto_increment primary key,
+    referencia varchar(100),
+    is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+
+create table fact_tipo_registro (
+    id_visita int not null,
+    id_tipo_registro int not null,
+    primary key (id_visita, id_tipo_registro),
+    constraint fk_fact_tipo_registro_visita foreign key (id_visita) 
+        references fact_visitas(id_visita),
+    constraint fk_fact_tipo_registro_tipo foreign key (id_tipo_registro) 
+        references dim_tipo_registro(id_tipo_registro)
+);
+
+create table fact_modo_atencion (
+    id_visita int not null,
+    id_modo_atencion int not null,
+    primary key (id_visita, id_modo_atencion),
+    constraint fk_fact_modo_atencion_visita foreign key (id_visita) 
+        references fact_visitas(id_visita),
+    constraint fk_fact_modo_atencion_modo foreign key (id_modo_atencion) 
+        references dim_modo_atencion(id_modo_atencion)
+);
+
+create table fact_visitas (
+    id_visita int auto_increment primary key,
+    id_cliente int,
+    id_proyecto int,
+    descripcion varchar(100),
+    id_categoria_medio int,
+    descripcion varchar(100),
+    id_medio int,
+    id_motivo_compra int,
+    presupuesto_disponible decimal(10,2),
+    id_referencia int,
+    otro_texto varchar(100),
+    is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+)
+
 
 /*
 drop table fact_clientes;
+drop table dim_tipo_registro;
+drop table dim_modo_atencion;
+drop table dim_motivo_compra;
+drop table dim_referencias;
+drop table fact_visitas;
 */
 
+insert into dim_motivo_compra (motivo_compra) 
+values ('Primera vivienda'),
+('Segunda vivienda'),
+('Inversión');
 
+insert into dim_referencias (referencia) 
+values ('No ha recibido referencias'),
+('Ha recibido referencias POSITIVAS'),
+('Ha recibido referencias NEGATIVAS');
+
+insert into dim_tipo_registro (tipo_registro) 
+values ('Presencial'),
+('Telefónico'),
+('WhatsApp'),
+('Email'),
+('Videollamada');
+
+insert into dim_modo_atencion (modo_atencion) 
+values ('Rápida'),
+('Información'),
+('Cierre'),
+('Trámites'),
+('Otro');
 
 
