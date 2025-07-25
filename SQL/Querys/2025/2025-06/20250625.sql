5c1ca1da77b2ea6f00f331e2394b46aae529f8f3
Alejandro Salcedo Bernal <asber_21@hotmail.com>
2025-07-25 09:27:20 -0500

    Actualizo query de unidades

---
+++ SQL/Querys/2025/2025-06/20250625.sql
@@ -0,0 +1,81 @@
+﻿create table fact_torres(
+	id_torre int not null auto_increment,
+	constraint pk_id_torre primary key(id_torre),
+	id_proyecto int not null,
+	constraint fk_id_proyecto_fact_torres foreign key(id_proyecto) references fact_proyectos(id_proyecto),
+	nombre_torre varchar(50),
+	consecutivo int,
+	is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+create index ix_id_proyecto_fact_torres on fact_torres(id_proyecto);
+create table dim_estado_unidad(
+	id_estado_unidad int not null,
+	constraint pk_id_estado_unidad primary key(id_estado_unidad),
+	estado_unidad varchar(100),
+	estado_unidad_plural varchar(100),
+	color_fondo varchar(20),
+	color_fuente varchar(20),
+	is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+insert into dim_estado_unidad(id_estado_unidad, estado_unidad, estado_unidad_plural, color_fondo, color_fuente) values
+(1, 'Libre','Libres', '#FFF', '#000'),
+(2, 'Opcionado','Opcionados', '#0094b9', '#FFF'),
+(3, 'Consignado','Consignados', '#0c62a4', '#FFF'),
+(4, 'Vendido','Vendidos', '#173d5b', '#FFF');
+
+create table dim_cuenta_convenio(
+	id_cuenta_convenio int not null auto_increment,
+	constraint pk_id_cuenta_convenio primary key(id_cuenta_convenio),
+	cuenta_tipo varchar(50),
+	cuenta_numero varchar(50),
+	convenio varchar(50),
+	cuota_inicial_banco varchar(50),
+	ean varchar(50),
+	is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
+create table fact_unidades(
+	id_unidad int not null auto_increment,
+	constraint pk_id_unidad primary key(id_unidad),
+	id_proyecto int not null,
+	constraint fk_id_proyecto_fact_unidades foreign key(id_proyecto) references fact_proyectos(id_proyecto),
+	id_torre int not null,
+	constraint fk_id_torre_fact_unidades foreign key(id_torre) references fact_torres(id_torre),
+	id_estado_unidad int,
+	constraint fk_id_estado_unidad_fact_unidades foreign key(id_estado_unidad) references dim_estado_unidad(id_estado_unidad),
+	nombre_unidad varchar(50),
+	numero_apartamento int,
+	piso int,
+	tipo varchar(50),
+	codigo_planta varchar(50),
+	localizacion varchar(50),
+	observacion_apto varchar(500),
+	fecha_fec date,
+	fecha_edi date,
+	fecha_edi_mostrar date,
+	inv_terminado bit,
+	num_alcobas int,
+	num_banos int,
+	area_privada_cub decimal(20, 2),
+	area_privada_lib decimal(20, 2),
+	area_total decimal(20, 2),
+	acue  decimal(20, 2),
+	area_total_mas_acue  decimal(20, 2),
+	valor_separacion  decimal(20, 2),
+	valor_acabados  decimal(20, 2),
+	valor_reformas  decimal(20, 2),
+	valor_descuento  decimal(20, 2),
+	pate varchar(50),
+	id_cuenta_convenio int,
+	constraint fk_id_cuenta_convenio_fact_unidades foreign key(id_cuenta_convenio) references dim_cuenta_convenio(id_cuenta_convenio),
+	asoleacion varchar(50),
+	altura varchar(50),
+	is_active bit default 1,
+	created_on datetime default current_timestamp,
+	created_by varchar(200) default current_user
+);
--- SQL/Unidades/ins_unidades.sql
+++ SQL/Unidades/ins_unidades.sql
@@ -2,5 +2,111 @@
 -- Proceso: /
 -- =============================================
 --START_PARAM
-set @Usuario = ''
+set @id_proyecto = 1,
+    @unidades = '{}',
+    @Usuario = '';
 --END_PARAM
+drop table if exists tmp_unidades;
+create temporary table tmp_unidades as(
+    select *, convert(null, int) as id_torre, convert(null, int) as id_cuenta_convenio, convert(null, int) as id_unidad
+    from json_table(@unidades, '$[*].pisos[*].unidades[*]' columns( 
+        apartamento varchar(50) path '$."apartamento"',
+        torre varchar(50) path '$."torre"',
+        piso varchar(50) path '$."piso"',
+        estatus varchar(50) path '$."estatus"',
+        tipo varchar(50) path '$."tipo"',
+        codigo_planta varchar(50) path '$."codigo_planta"',
+        localizacion varchar(50) path '$."localizacion"',
+        observacion_apto varchar(500) path '$."observacion_apto"',
+        fecha_fec varchar(50) path '$."fecha_fec"',
+        fecha_edi varchar(50) path '$."fecha_edi"',
+        fecha_edi_mostrar varchar(50) path '$."fecha_edi_mostrar"',
+        inv_terminado varchar(50) path '$."inv_terminado"',
+        num_alcobas varchar(50) path '$."num_alcobas"',
+        num_banos varchar(50) path '$."num_banos"',
+        area_privada_cub varchar(50) path '$."area_privada_cub"',
+        area_privada_lib varchar(50) path '$."area_privada_lib"',
+        area_total varchar(50) path '$."area_total"',
+        acue varchar(50) path '$."acue"',
+        area_total_mas_acue varchar(50) path '$."area_total_mas_acue"',
+        lista varchar(50) path '$."lista"',
+        valor_separacion varchar(50) path '$."valor_separacion"',
+        valor_acabados varchar(50) path '$."valor_acabados"',
+        valor_reformas varchar(50) path '$."valor_reformas"',
+        valor_descuento varchar(50) path '$."valor_descuento"',
+        pate varchar(50) path '$."pate"',
+        cuenta_tipo varchar(50) path '$."cuenta_tipo"',
+        cuenta_numero varchar(50) path '$."cuenta_numero"',
+        convenio varchar(50) path '$."convenio"',
+        cuota_inicial_banco varchar(50) path '$."cuota_inicial_banco"',
+        ean varchar(50) path '$."ean"',
+        asoleacion varchar(50) path '$."asoleacion"',
+        altura varchar(50) path '$."altura"'
+    ))  as a
+);
+--TODO hacer validación de datos antes de continuar
+insert into fact_torres(id_proyecto, nombre_torre, consecutivo, created_by)
+select distinct @id_proyecto, concat('Torre ', torre), torre, @usuario
+from tmp_unidades a
+    left join fact_torres b on b.id_proyecto = @id_proyecto and a.torre = b.consecutivo
+where b.id_torre is null;
+
+update tmp_unidades a
+    join fact_torres b on a.torre = b.consecutivo
+set 
+    a.id_torre = b.id_torre
+where b.id_proyecto = @id_proyecto;
+
+
+insert into dim_cuenta_convenio(cuenta_tipo, cuenta_numero, convenio, cuota_inicial_banco, ean, created_by)
+select distinct a.cuenta_tipo, a.cuenta_numero, a.convenio, a.cuota_inicial_banco, a.ean, @created_by
+from tmp_unidades a
+    left join dim_cuenta_convenio b on 
+        a.cuenta_tipo = b.cuenta_tipo and a.cuenta_numero = b.cuenta_numero and a.convenio = b.convenio
+            and a.cuota_inicial_banco = b.cuota_inicial_banco and a.ean = b.ean
+where b.id_cuenta_convenio is null;
+
+update tmp_unidades a
+    join dim_cuenta_convenio b on a.cuenta_tipo = b.cuenta_tipo and a.cuenta_numero = b.cuenta_numero and a.convenio = b.convenio
+            and a.cuota_inicial_banco = b.cuota_inicial_banco and a.ean = b.ean
+set 
+    a.id_cuenta_convenio = b.id_cuenta_convenio;
+
+insert into fact_unidades(
+	id_proyecto, id_torre, id_estado_unidad, nombre_unidad, numero_apartamento, piso, tipo, codigo_planta, localizacion, observacion_apto, fecha_fec,
+	fecha_edi, fecha_edi_mostrar, inv_terminado, num_alcobas, num_banos, area_privada_cub, area_privada_lib, area_total, acue, area_total_mas_acue,
+	valor_separacion, valor_acabados, valor_reformas, valor_descuento, pate, id_cuenta_convenio, asoleacion, altura, created_by 
+) select distinct
+    @id_proyecto as id_proyecto,
+	id_torre as id_torre,
+	null as id_estado_unidad,
+	concat('Apto ', apartamento) as nombre_unidad,
+	convert(apartamento, int) as numero_apartamento,
+	convert(piso, int) as piso,
+	tipo as tipo,
+	codigo_planta as codigo_planta,
+	localizacion as localizacion,
+	left(observacion_apto, 500) as observacion_apto,
+	convert(fecha_fec, date) as fecha_fec,
+	convert(fecha_edi, date) as fecha_edi,
+	convert(fecha_edi_mostrar, date) as fecha_edi_mostrar,
+	convert(inv_terminado, unsigned) as inv_terminado,
+	convert(num_alcobas, int) as num_alcobas,
+	convert(num_banos, int) as num_banos,
+	convert(area_privada_cub, decimal(20, 2)) as area_privada_cub,
+	convert(area_privada_lib, decimal(20, 2)) as area_privada_lib,
+	convert(area_total, decimal(20, 2)) as area_total,
+	convert(acue, decimal(20, 2)) as acue,
+	convert(area_total_mas_acue, decimal(20, 2)) as area_total_mas_acue,
+	convert(valor_separacion, decimal(20, 2)) as valor_separacion,
+	convert(valor_acabados, decimal(20, 2)) as valor_acabados,
+	convert(valor_reformas, decimal(20, 2)) as valor_reformas,
+	convert(valor_descuento, decimal(20, 2)) as valor_descuento,
+	pate as pate,
+	id_cuenta_convenio as id_cuenta_convenio,
+	asoleacion as asoleacion,
+	altura as altura,
+	@usuario as created_by
+from tmp_unidades;
+
+select 'OK' as respuesta;
