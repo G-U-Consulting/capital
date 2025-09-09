eb7c7ad37d4bac347a9b86d68cf67fb4beddf5af
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2025-09-08 20:40:41 -0500

    cotizacion

--- SQL/ProcesoNegocio/Get_Unidades_Cotizacion.sql
+++ SQL/ProcesoNegocio/Get_Unidades_Cotizacion.sql
@@ -3,11 +3,13 @@
 -- =============================================
 --START_PARAM
 set @id_cliente = '2',
-    @id_proyecto = '3';
+    @id_proyecto = '3',
+    @cotizacion = '1';
 --END_PARAM
 
 select *
 from fact_negocios_unidades
 where id_cliente = @id_cliente
   and id_proyecto = @id_proyecto
+  and cotizacion = @cotizacion
   and is_active = 1;
