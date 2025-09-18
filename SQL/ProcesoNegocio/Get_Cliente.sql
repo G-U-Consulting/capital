fbffdcf263064cf9c61fc90cc77368d477268c9a
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2025-09-17 21:30:26 -0500

    Avances Cotizacion

--- SQL/ProcesoNegocio/Get_Cliente.sql
+++ SQL/ProcesoNegocio/Get_Cliente.sql
@@ -2,36 +2,40 @@
 -- Proceso: ProcesoNegocio/Get_Cliente
 -- =============================================
 --START_PARAM
-
-set @cliente = '';
+set @cliente = '222222';
 --END_PARAM
 
-select a.id_cliente,
-      a.nombres,
-      a.apellido1,
-      a.apellido2,
-      a.direccion,
-      a.ciudad,
-      a.barrio,
-      a.departamento,
-      a.pais,
-      a.email1,
-      a.email2,
-      a.telefono1,
-      a.telefono2,
-      a.tipo_documento,
-      a.numero_documento,
-      a.pais_expedicion,
-      a.departamento_expedicion,
-      a.ciudad_expedicion,
-      a.is_politica_aceptada,
-      a.is_vetado,
-      date_format(a.fecha_expedicion, '%Y-%m-%d') as fecha_expedicion
+select 
+    a.id_cliente,
+    a.nombres,
+    a.apellido1,
+    a.apellido2,
+    a.direccion,
+    a.ciudad,
+    a.barrio,
+    a.departamento,
+    a.pais,
+    a.email1,
+    a.email2,
+    a.telefono1,
+    a.telefono2,
+    a.tipo_documento,
+    a.numero_documento,
+    a.pais_expedicion,
+    a.departamento_expedicion,
+    a.ciudad_expedicion,
+    a.is_politica_aceptada,
+    a.is_vetado,
+    DATE_FORMAT(a.fecha_expedicion, '%Y-%m-%d') as fecha_expedicion,
+    DATE_FORMAT(a.fecha_nacimiento, '%Y-%m-%d') as fecha_nacimiento,
+    a.nit,
+    a.is_titular,
+    a.nombre_empresa
 from fact_clientes a
 where a.is_active = 1
   and @cliente != ''
   and (
-    a.nombres like concat('%', @cliente collate utf8mb4_general_ci, '%')
-    or a.numero_documento = @cliente
+    a.nombres COLLATE utf8mb4_unicode_ci like CONCAT('%', @cliente, '%') COLLATE utf8mb4_unicode_ci
+    or a.numero_documento COLLATE utf8mb4_unicode_ci = @cliente COLLATE utf8mb4_unicode_ci
   )
-order by 1;
+order by a.id_cliente;
