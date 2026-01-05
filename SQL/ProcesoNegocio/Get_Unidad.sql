618cd38f81f304f466053ee5a346e184c1b71751
Ruben Piedrahita <ruben.piedrahita@serlefin.com>
2026-01-05 16:15:42 -0500

    Clases Pdf

--- SQL/ProcesoNegocio/Get_Unidad.sql
+++ SQL/ProcesoNegocio/Get_Unidad.sql
@@ -5,7 +5,8 @@
 set @id_unidad = '91032';
 
 --END_PARAM
-select *
+select a.*, b.*, tp.tipo_proyecto as clase
 from fact_unidades a
 join fact_proyectos b on a.id_proyecto = b.id_proyecto
+join dim_tipo_proyecto tp on a.id_clase = tp.id_tipo_proyecto
 where id_unidad = @id_unidad;
